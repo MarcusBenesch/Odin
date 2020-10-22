1251251550c668aa1e447112e98e41b1fcd113e8
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2020-10-22 13:01:22 -0500

    Finish Copying from pdy

--- udpclient.c
+++ udpclient.c
@@ -33,5 +33,16 @@ int main( int argc, char* argv[])
     servAddr.sin_family = AF_INET;
     servAddr.sinport = htons(SERVER_PORT);
     serveAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if((s = socket(PF_INET,SOCK_DGRAM, 0))<0)
+    {
+        perror("Error: socket failed!");
+        exit(1);
+    }
 
+    len = sendto(s, string, strlen(string), 0, (struct sockaddr)&servAddr,sizeof(servAddr));
+    recvfrom(s, buffer, len, 0, NULL, NULL);
+    buffer[len]= '\0';
+    printf(" Echo string recieved: %s", buffer);
+    close(s);
+    exit(0);
 }
