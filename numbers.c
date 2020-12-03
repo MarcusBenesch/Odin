67f36e632fe20649fe6d15f5fb38ad130b819f8b
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2020-12-02 19:04:44 -0600

    jkl;

--- numbers.c
+++ numbers.c
@@ -9,15 +9,15 @@ char word [8];
 
 int main( int ac, char *av[] )
 {
-    char temp[7] = {'1', '2', '3', '4', '5', '6', '7'};
-    generate(temp);
+    int numbers[7] = {2,4,0,1,1,7,9};
+    generate(numbers);
 }
 //This function takes in 7 numbers, which are the 7 digits of the phone number and generates all possible words.
-void generate( char num[7])
+void generate( int num[7])
 {
-    printf("Test %s", dial[2]);
-    for(int a =0; a < strlen(dial[2]); a++)
+    printf("number [0] = %d, Sting = %s",num[0], dial[num[0]]);
+    for(int a =0; a < strlen(dial[num[0]]); a++)
     {
-        printf("%c", dial[2][a]);
+        printf("%c", dial[num[0]][a]);
     }
 }
