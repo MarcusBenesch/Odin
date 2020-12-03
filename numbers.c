02cb294882d3e0b4c9735be73764b38f56d443c2
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2020-12-02 22:08:42 -0600

    nkl;

--- numbers.c
+++ numbers.c
@@ -99,12 +99,12 @@ int checkword(char input[8])
             if(strcmp(buffer, input) < 0)
             {
                 l = mid;
-                mid = mid /2;
+                int mid = (l + r)/2;
             }
             if(strcmp(buffer, input) > 0)
             {
                 r = mid;
-                mid = mid /2;
+                int mid = (l + r)/2;
             }
             if(strcmp(buffer, input) == 0)
             {
