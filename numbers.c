f1ea55c2b39c62087afaa681c1c404c8fe587c01
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2020-12-02 23:50:46 -0600

    mkl;

--- numbers.c
+++ numbers.c
@@ -10,37 +10,7 @@ int checkword();
 int main( int ac, char *av[] )
 {   
 
-    char temp[12];
-    int phoneNum[7];
-    
-    printf("Enter a phone number: ");
-    while( (fgets(temp,13,stdin)) != NULL)
-    {
-        
-        //validate input char by char
-        if(!isdigit(temp[0])||!isdigit(temp[1])||!isdigit(temp[2])||!isdigit(temp[4])||!isdigit(temp[5])||!isdigit(temp[6])||!isdigit(temp[8])||!isdigit(temp[9])||!isdigit(temp[10])||!isdigit(temp[11])|| temp[3] != '-'|| temp[7]!= '-')
-        {
-            printf("%s\n", temp);
-            printf("Invalid number, try again. The phone number should be 10 digits separated by dashes. ");
-        }
-        else
-        {
-            printf("xxx %s\n", temp);
-            phoneNum[0] = temp[4] -48;
-            phoneNum[1] = temp[5] -48;
-            phoneNum[2] = temp[6] -48;
-            phoneNum[3] = temp[8] -48;
-            phoneNum[4] = temp[9] -48;
-            phoneNum[5] = temp[10] -48;
-            phoneNum[6] = temp[11] -48;
-            //Debug purposes
-            //printf("%d%d%d-%d%d%d%d", phoneNum[0],  phoneNum[1],  phoneNum[2],  phoneNum[3],  phoneNum[4],  phoneNum[5],  phoneNum[6]); 
-            generate(phoneNum);
-            
-        }
-        
-        
-    }
+    printf("%d",checkword("CREWMEN"));
     
 
 }
@@ -135,7 +105,7 @@ int checkword(char input[8])
                 buffer[x] = toupper(buffer[x]);
             }
             //Debugging information
-            printf("buffer = %s, input = %s, l = %d, r = %d, mid = %d\n", buffer, input, l, r, mid);
+            //printf("buffer = %s, input = %s, l = %d, r = %d, mid = %d\n", buffer, input, l, r, mid);
             if(strcmp(buffer, input) < 0)
             {
                 l = mid;
