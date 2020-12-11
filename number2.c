eaaba495a15957f7525904e4af5527013bc9e9e4
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2020-12-11 14:27:01 -0600

    nkl;

--- number2.c
+++ number2.c
@@ -114,7 +114,7 @@ int insertNode(node ** root)
 
 void insert(node ** tree, char data [8])
 {
-    printf("Inserting %s into %d", data, tree);
+    printf("Inserting %s into %d\n", data, tree);
     node *temp = NULL;
     if(!(*tree))
     {
