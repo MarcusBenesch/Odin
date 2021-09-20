33811eaafef1fc0601c6598226f9e3b72f01fbfe
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2021-09-19 21:50:21 -0500

    first

---
+++ hello.c
@@ -0,0 +1,20 @@
+/*
+ * Auth: Marcus Benesch
+ * Date: 09-19-2021
+ * Course: CSCI-4500 (Sec: 002)
+ * Desc:  PREPROJECT-01, A Simple "Hello, World" C program.
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main( void )  {
+
+	/* Personalized hello. */
+	printf( "Hello, World, from Marcus Benesch!\n" );
+
+	/* Proper way to finish a program in a POSIX-system. */
+	exit( EXIT_SUCCESS );
+
+} /* end main()
