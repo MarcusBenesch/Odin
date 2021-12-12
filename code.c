bf549db0885334b253702c25871d1274c605aae4
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2021-12-12 16:53:46 -0600

    simCom

--- code.c
+++ code.c
@@ -36,7 +36,7 @@ int main(void)
     char *args[] = {"-l", "-a", (char *)0};
     forkFunc("/usr/bin/ls", args);
 
-   args[] = {"", (char *)0};
+   args = {"", (char *)0};
     forkFunc("/usr/bin/ls", args);
     return 0;
 }
