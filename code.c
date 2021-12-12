0808850a27cb19323b561d2c8c4b375da7b1fd52
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2021-12-12 16:19:50 -0600

    simCom

--- code.c
+++ code.c
@@ -30,7 +30,7 @@ int forkFunc( const char *str, char *args[])
         char *env_args[] = { (char *)0};
         printf("child");
         //execve(str, args, env_args);
-        _exit(EXIT_FAILURE); // exec never returns
+        //_exit(EXIT_FAILURE); // exec never returns
     }
 }
 int main(void)
