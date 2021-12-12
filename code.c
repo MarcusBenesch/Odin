e1a425e451730715a1528d47d6a19bd3b1632435
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2021-12-12 16:37:56 -0600

    simCom

--- code.c
+++ code.c
@@ -28,7 +28,7 @@ int forkFunc( void)
     {
         char *args[] = {"-l", "-a", (char *)0};
         char *env_args[] = {(char*)0};
-        execve("ls", args, env_args);
+        execve("/usr/bin/ls", args, env_args);
         printf("child");
         //execve("ls", _args, env_args);
         _exit(EXIT_FAILURE); // exec never returns
