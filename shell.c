c5e2c0aa7a06185a86c23176b12ea1a54dfcc507
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2021-12-12 17:49:58 -0600

    simCom

--- shell.c
+++ shell.c
@@ -330,7 +330,7 @@ int main(void)
                     printf("Sim Com 1 = %s\n", cmd);
                     //printf("path1 = %s", path1);
                     int bool = findPath(tp,cmd, path1);
-                    strcpy(tp,path);
+                    //strcpy(tp,path);
                     //printf("path = %s\n", path1);
                     if (bool == 0)
                     {
