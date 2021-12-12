84c411f357878d0d928adfaef2e5629f22306db3
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2021-12-12 16:11:49 -0600

    simCom

--- shell.c
+++ shell.c
@@ -12,6 +12,32 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 /*#include"csci4500utils.h"*/
+int forkFunc( const char *str, char *args[])
+{
+    pid_t parent = getpid();
+    pid_t pid = fork();
+    printf("test\n");
+
+    if (pid == -1)
+    {
+        // error, failed to fork()
+        printf("Failed");
+    }
+    else if (pid > 0)
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        printf("I am parent\n");
+    }
+    else
+    {
+        
+        char *env_args[] = { (char *)0};
+        execve(str, args, env_args);
+        _exit(EXIT_FAILURE); // exec never returns
+    }
+}
+
 int findPath(const char *str, char *buf)
 {
 char *path = NULL;
@@ -292,12 +318,15 @@ int main(void)
                         //writestring( 1, tempString);
                     }
                     char path1[90];
-                    printf("debug1\n");
-                    printf("Sim Com 1 = %s", cmd);
+                    //printf("debug1\n");
+                    //printf("Sim Com 1 = %s", cmd);
                     int bool = findPath(cmd, path1);
                     if (bool == 0)
                     {
+                        argsOfSM[argCount] = (char *)0;
+                        forkFunc(cmd, argsOfSM);
                         printf("string = %s", path1);
+                        
                     }
                     else
                     {
