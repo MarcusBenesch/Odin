42dd120d6f3cb1b3a42762b158c0a684fe348b4d
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2021-12-12 17:35:44 -0600

    simCom

--- code.c
+++ code.c
@@ -5,15 +5,15 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 /*#include"csci4500utils.h"*/
-int findPath(char *path,const char *str, char *buf)
+int findPath(const char *str, char *buf)
 {
-
+char *path = NULL;
     char *tok;         
     char *delim = ":";
     int count = 0;
     int x;
 
-    
+    path = getenv( "PATH" );
 
     if( path != NULL ) {   
         x = access( str, X_OK); 
@@ -30,12 +30,9 @@ int findPath(char *path,const char *str, char *buf)
             strcpy(temp, tok);
             strcat(temp, "/");
             strcat(temp, str);
-            printf("s = %s\n", temp );
             x = access( temp, X_OK); 
-            printf("x = %d\n", x);
             if(x == 0)
-            {   
-                
+            {
                 strcpy(buf, temp);
                 return 0;
             }        
@@ -45,18 +42,12 @@ int findPath(char *path,const char *str, char *buf)
     }
     return 1;
 }
-
 int main(void)
 {
 
-    int x = access( "/usr/bin/ls", X_OK); 
-    printf("x = %d\n", x);
-    x = access( "/usr/bin/ls", X_OK); 
-    printf("x = %d\n", x);
-    char path1[90];
-    char *path = getenv( "PATH" );
-    int bool = findPath(path, "ls", path1);
-    path = getenv( "PATH" );
+    ;
+    
+    int bool = findPath("ls", path1);
     printf("//////////////////////////////");
     int bool2 = findPath(path,"ls", path1);
 
