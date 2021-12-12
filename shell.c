e2ccf3423c855b4410ab8034b6b8b110c52e1559
MarcusBenesch <56902285+MarcusBenesch@users.noreply.github.com>
2021-12-12 15:08:42 -0600

    simCom

--- shell.c
+++ shell.c
@@ -292,9 +292,9 @@ int main(void)
                     
                     printf("Sim Com 1 = %s", cmd);
                     
-                    //findPath(sm, path1);
+                    findPath(sm, path1);
                     printf("\n///////////////////////////////////////////////////\n");
-                    //printf("%s", path1);
+                    printf("%s", path1);
                     sprintf(tempString, "\n\n");
                     writestring( 1, tempString);
                 }
